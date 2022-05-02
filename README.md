b8ce3aae7b3808b73e8eb6ba510d0d8ce8173f5a
Darkempire <50015928+Darkempire78@users.noreply.github.com>
2022-05-02 23:41:41 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# StackOverflow Snippets
+
+A browser extension that adds code snippets from StackOverflow to search results
+
+# Preview
+
+![](screenshot.png)
+
+This project is derived from [codesearch](https://github.com/busybox11/codesearch)
---
+++ content.js
@@ -0,0 +1,52 @@
+for (let i = document.getElementsByClassName('yuRUbf').length; i-- > 0;) {
+	let link = document.getElementsByClassName('yuRUbf')[i].getElementsByTagName('a')[0]
+	if (link.host == "stackoverflow.com" && link.pathname.startsWith('/questions/')) {
+		let id = link.pathname.replace('/questions/', '')
+		id = id.substr(0, id.indexOf('/'))
+
+		let rescont = document.getElementsByClassName('IsZvec')[i]
+
+		// Request to StackOverflow's API to get the answers
+		fetch(`https://api.stackexchange.com/2.2/questions/${id}/answers?order=desc&sort=votes&site=stackoverflow&filter=!b6AubVkmmBt14D`).then(data => {
+			data = data.json()
+			console.log(data)
+			data.then(function(data) {
+				if (data.quota_remaining > 0) {
+					let otherlinks = ""
+					for (let j = rescont.getElementsByTagName('div').length; j-- > 0;) {
+						if (rescont.getElementsByTagName('div')[j].dataset.ved != undefined) {
+							otherlinks = '<span class="cardInfo"><b>Related links</b></span>' + rescont.getElementsByTagName('div')[j].outerHTML
+						}
+					}
+					ans = data.items[0]
+
+					let type
+					if (ans.is_accepted) {
+						type = "Verified"
+					} else {
+						type = "Top"
+					}
+					let body = ans.body
+					let score = ans.score
+
+					rescont.innerHTML = `
+						<div class="card">
+							<div class="cardContent">
+								<span class="cardInfo">${type} solution - Score: ${score}</span>
+								<div class="cardAnswer">${body}</div>
+							</div>
+							<div class="otherLinks">${otherlinks}</div>
+						</div>
+						
+					`
+
+					rescont.querySelectorAll('pre code').forEach((block) => {
+						hljs.highlightBlock(block)
+					})
+				} else {
+					rescont.innerHTML = "<i>codesearch is rate-limited by StackOverflow's API. The limitation is reset every day.</i><br>" + rescont.innerHTML
+				}
+			})
+		})
+	}
+}
